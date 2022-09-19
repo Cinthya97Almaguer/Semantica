92fb5a84f378d240ea30279f5d5b4b28748c60b2
Cinthya97Almaguer <cris008alma@gmail.com>
2022-09-19 09:38:35 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Semantica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenguaje a = new Lenguaje();
+
+                a.Programa();
+
+
+                
+                /*a.match("#");
+                a.match("include");
+                a.match("<");
+                a.match(Token.Tipos.Identificador);
+                a.match(".");
+                a.match("h");
+                a.match(">"); */
+                
+                //while(!a.FinArchivo())
+                //{
+                  //  a.NextToken();
+                //}
+                a.cerrar();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Fin de compilacion");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
